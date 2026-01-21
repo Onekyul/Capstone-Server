fffd4094ccca0269cd2f1b1fdcbda8bc502d2b17
Onekyul <153491652+Onekyul@users.noreply.github.com>
2026-01-22 01:38:25 +0900

    [feat] ChatController 구현
    유니티 클라이언트로 부터 메세지를 받아서 Redis에 뿌려주는 Send API 구현

---
+++ GameServer/Controllers/ChatController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+using System.Threading.Tasks;
+
+namespace GameServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+        private readonly IConnectionMultiplexer _redis;
+
+        public ChatController(IConnectionMultiplexer redis)
+        {
+            _redis = redis;
+        }
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMessage([FromBody] ChatMessageReq req)
+        {
+            var db = _redis.GetDatabase();
+            var sub = _redis.GetSubscriber();
+
+            string formattingMessage = $"{req.Nickname}:{req.Message}";
+            await sub.PublishAsync("chat:global", formattingMessage);
+            return Ok();
+        }
+    }
+}
+public class ChatMessageReq
+{
+    public string Nickname { get; set; }
+    public string Message { get; set; }
+}
