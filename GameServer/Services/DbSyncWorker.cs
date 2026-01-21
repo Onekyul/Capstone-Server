b912b73ecea50076770e57facb85dbc13ce18586
Onekyul <153491652+Onekyul@users.noreply.github.com>
2026-01-21 22:28:35 +0900

    [refactor] Write-Back 백그라운드 워커 Batch Processing 적용
    1초에 한번 수행했던 백그라운드 워커 Batch 사이즈 적용해서 한번에 여러 명 Write-Back 가능

--- GameServer/Services/DbSyncWorker.cs
+++ GameServer/Services/DbSyncWorker.cs
@@ -4,6 +4,7 @@ using GameServer.Models;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
 using System.ComponentModel;
+using System.Runtime.InteropServices;
 using System.Text.Json;
 
 namespace GameServer.Services
@@ -13,6 +14,8 @@ namespace GameServer.Services
         private readonly IConnectionMultiplexer _redis;
         private readonly IServiceProvider _serviceProvider; // DB 연결을 위한 도구
         private readonly ILogger<DbSyncWorker> _logger;
+
+        private const int batchSize = 50; // 한 번
 
         public DbSyncWorker(IConnectionMultiplexer redis, IServiceProvider serviceProvider, ILogger<DbSyncWorker> logger)
         {
@@ -30,18 +33,33 @@ namespace GameServer.Services
             {
                 try
                 {
-                    //Redis 큐에서 task 하나 꺼내기 (RPOP)
-                    RedisValue userId = await db.ListRightPopAsync("task:writeback");
+                    //Redis 큐에서 배치 만큼 꺼내기
+                    //StackExchange.Redis는 pop을 한번 씩 -> 반복문 사용
+                    List<string> userIds = new List<string>();
 
-                    if (userId.HasValue)
+                    for(int i=0; i < batchSize; i++)
                     {
-                        _logger.LogInformation($"[Sync] 유저 {userId} 데이터 저장 시작");
-                        await ProcessSaveAsync(db, userId.ToString());
+                        var redisValue = await db.ListRightPopAsync("task:writeback");
+                        if (!redisValue.HasValue) break;
+
+                        string uid = redisValue.ToString();
+                        if (!userIds.Contains(uid))
+                        {
+                            userIds.Add(uid);
+                        }
+                    }
+
+                    if (userIds.Count > 0)
+                    {
+                        _logger.LogInformation($"[Batch Save] {userIds.Count}명의 데이터 저장 시작");
+                        await ProcessBatchSaveAsync(db, userIds);
                     }
                     else
                     {
+                        //큐가 비었을때만 1초 Delay
                         await Task.Delay(1000, stoppringToken);
                     }
+                  
                 }
                 catch (Exception ex)
                 {
@@ -52,74 +70,77 @@ namespace GameServer.Services
         }
      
         //Redis_to_DB 저장 로직
-        private async Task ProcessSaveAsync(IDatabase redisDb, string userId)
+        private async Task ProcessBatchSaveAsync(IDatabase redisDb, List<string> userIds)
         {
-            string json = await redisDb.StringGetAsync($"user:{userId}:data");
-            if (string.IsNullOrEmpty(json)) return;
-
-            var clientData= JsonSerializer.Deserialize<GameDataDto>(json);
-
+           
             using (var scope = _serviceProvider.CreateScope())
             {
                 var _context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
 
-                // 1. 유저 조회
-                var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == int.Parse(userId));
-                if (user == null) return; // 유저가 없으면 스킵
-
-                // 2. 기본 정보 업데이트
-                user.MaxClearedStage = clientData.stage;
-                if (clientData.equip != null)
+                foreach(var userId in userIds)
                 {
-                    user.EquippedWeaponId = clientData.equip.weapon;
-                    user.EquippedHelmetId = clientData.equip.helmet;
-                    user.EquippedArmorId = clientData.equip.armor;
-                    user.EquippedBootsId = clientData.equip.boots;
-                }
+                    string json = await redisDb.StringGetAsync($"user:{userId}:data");
+                    if (string.IsNullOrEmpty(json)) continue;
 
-                // 3. 인벤토리 저장 (기존 삭제 -> 새로 추가 방식)
-                var oldItems = _context.UserItems.Where(i => i.UserId == user.Id);
-                _context.UserItems.RemoveRange(oldItems);
-                foreach (var itemDto in clientData.inventory)
-                {
-                    _context.UserItems.Add(new UserItem
+                    var clientData = JsonSerializer.Deserialize<GameDataDto>(json);
+                    
+                    // 유저 정보 조회
+                    var user = await _context.Users.FirstOrDefaultAsync(u => u.Id == int.Parse(userId));
+                    if (user == null) return; // 유저가 없으면 스킵
+
+                    user.MaxClearedStage = clientData.stage;
+                    if (clientData.equip != null)
                     {
-                        UserId = user.Id,
-                        ItemId = itemDto.id,
-                        Count = itemDto.count
-                    });
-                }
+                        user.EquippedWeaponId = clientData.equip.weapon;
+                        user.EquippedHelmetId = clientData.equip.helmet;
+                        user.EquippedArmorId = clientData.equip.armor;
+                        user.EquippedBootsId = clientData.equip.boots;
+                    }
 
-                // 4. 장비 저장
-                var oldEquips = _context.UserEquipments.Where(e => e.UserId == user.Id);
-                _context.UserEquipments.RemoveRange(oldEquips);
-                foreach (var equipDto in clientData.equipments)
-                {
-                    _context.UserEquipments.Add(new UserEquipment
+                    // 인벤토리 저장 
+                    var oldItems = _context.UserItems.Where(i => i.UserId == user.Id);
+                    _context.UserItems.RemoveRange(oldItems);
+                    foreach (var itemDto in clientData.inventory)
                     {
-                        UserId = user.Id,
-                        ItemId = equipDto.id,
-                        Level = equipDto.level
-                    });
-                }
+                        _context.UserItems.Add(new UserItem
+                        {
+                            UserId = user.Id,
+                            ItemId = itemDto.id,
+                            Count = itemDto.count
+                        });
+                    }
 
-                // 5. 인챈트 저장
-                var oldEnchants = _context.UserEnchants.Where(e => e.UserId == user.Id);
-                _context.UserEnchants.RemoveRange(oldEnchants);
-                foreach (var enchantDto in clientData.enchants)
-                {
-                    _context.UserEnchants.Add(new UserEnchant
+                    // 장비 저장
+                    var oldEquips = _context.UserEquipments.Where(e => e.UserId == user.Id);
+                    _context.UserEquipments.RemoveRange(oldEquips);
+                    foreach (var equipDto in clientData.equipments)
                     {
-                        UserId = user.Id,
-                        EnchantId = enchantDto.id,
-                        Level = enchantDto.level
-                    });
-                }
+                        _context.UserEquipments.Add(new UserEquipment
+                        {
+                            UserId = user.Id,
+                            ItemId = equipDto.id,
+                            Level = equipDto.level
+                        });
+                    }
 
-                // 6. 진짜 저장 (Commit)
+                    // 인챈트 저장
+                    var oldEnchants = _context.UserEnchants.Where(e => e.UserId == user.Id);
+                    _context.UserEnchants.RemoveRange(oldEnchants);
+                    foreach (var enchantDto in clientData.enchants)
+                    {
+                        _context.UserEnchants.Add(new UserEnchant
+                        {
+                            UserId = user.Id,
+                            EnchantId = enchantDto.id,
+                            Level = enchantDto.level
+                        });
+                    }
+
+                }
+            
                 await _context.SaveChangesAsync();
 
-                _logger.LogInformation($"[Sync Complete] 유저 {userId} DB 저장 완료.");
+                _logger.LogInformation($"[Batch Complete] 유저 {userIds.Count}명 DB 저장 완료.");
 
             }
 
