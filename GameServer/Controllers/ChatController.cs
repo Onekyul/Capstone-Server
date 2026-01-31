918c40bdbae9dfe7146ed860cae80a9454e428be
Onekyul <153491652+Onekyul@users.noreply.github.com>
2026-02-01 00:04:46 +0900

    [feat] 채팅 API 구현
    1초마다 polling하는 채팅 시스템 구현
    유니티와 연동될 수 있게 Send/Receive API 재구성 및 구현

--- GameServer/Controllers/ChatController.cs
+++ GameServer/Controllers/ChatController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using StackExchange.Redis;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace GameServer.Controllers
@@ -11,25 +13,55 @@ namespace GameServer.Controllers
     {
         private readonly IConnectionMultiplexer _redis;
 
+       
+        private const string CHAT_LIST_KEY = "Chat:History"; 
+        private const string CHAT_PUB_KEY = "Chat:Lobby";   
+
         public ChatController(IConnectionMultiplexer redis)
         {
             _redis = redis;
         }
 
+      
         [HttpPost("send")]
         public async Task<IActionResult> SendMessage([FromBody] ChatMessageReq req)
         {
             var db = _redis.GetDatabase();
             var sub = _redis.GetSubscriber();
 
-            string formattingMessage = $"{req.Nickname}:{req.Message}";
-            await sub.PublishAsync("chat:global", formattingMessage);
+         
+            string timeStamp = DateTime.Now.ToString("HH:mm");
+            string formattedMessage = $"<color=#AAAAAA>[{timeStamp}]</color> <color=yellow>{req.Nickname}</color> : {req.Message}";
+
+          
+            await db.ListRightPushAsync(CHAT_LIST_KEY, formattedMessage);
+            await db.ListTrimAsync(CHAT_LIST_KEY, -50, -1); // 최신 50개만 유지
+
+          
+            await sub.PublishAsync(CHAT_PUB_KEY, formattedMessage);
+
             return Ok();
         }
+
+       
+        [HttpGet("receive")]
+        public async Task<IActionResult> GetMessages()
+        {
+            var db = _redis.GetDatabase();
+
+            // List에 저장된 최근 메시지 20개  반환
+            var redisList = await db.ListRangeAsync(CHAT_LIST_KEY, -20, -1);
+
+            // RedisValue[] -> string[]
+            var messages = redisList.Select(x => x.ToString()).ToArray();
+
+            return Ok(messages);
+        }
     }
-}
-public class ChatMessageReq
-{
-    public string Nickname { get; set; }
-    public string Message { get; set; }
+
+    public class ChatMessageReq
+    {
+        public string Nickname { get; set; }
+        public string Message { get; set; }
+    }
 }
