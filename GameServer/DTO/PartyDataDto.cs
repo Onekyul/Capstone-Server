4d65c4b2dd31e341bf3c24cafa60ef1758eb177c
Onekyul <153491652+Onekyul@users.noreply.github.com>
2026-02-01 02:21:06 +0900

    [feat] 파티 컨트롤러 구현
    파티 시스템에 쓰이는 API및 DTO 구현

---
+++ GameServer/DTO/PartyDataDto.cs
@@ -0,0 +1,31 @@
+﻿namespace GameServer.DTO
+{
+          // 파티 생성 요청
+        public class PartyCreateReq
+        {
+            public string Title { get; set; }
+            public int LeaderId { get; set; }
+            public string LeaderNickname { get; set; }
+            public int MaxCount { get; set; } = 4;
+            public int DungeonId { get; set; } = 1;
+        }
+
+        // 파티 정보 응답
+        public class PartyDto
+        {
+            public int PartyId { get; set; }
+            public string Title { get; set; }
+            public string LeaderName { get; set; }
+            public int CurrentCount { get; set; }
+            public int MaxCount { get; set; }
+            public int DungeonId { get; set; }
+        }
+
+        // 던전 입장 요청 (아까 추가한 것)
+        public class PartyEnterReq
+        {
+            public int PartyId { get; set; }
+            public int UserId { get; set; }
+        }
+   
+}
